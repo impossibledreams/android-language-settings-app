33a2d4f550176c4c0ab65e7382648309a3fa9c12
Yakov Shafranovich <yakovsh@shaftek.biz>
2013-06-02 22:08:12 -0400

    added privacy policy

---
+++ PRIVACY.markdown
@@ -0,0 +1,3 @@
+Privacy Policy for the Android Language Settings App
+====================================================
+This application does not collect or use any personal data.
---
+++ android-language-settings-app/gen/appinventor/ai_yakov/LanguageSettings/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package appinventor.ai_yakov.LanguageSettings;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
